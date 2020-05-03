d9871587e3ad531d23f5faee9251defe3a33fba1
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-05-02 22:53:30 -0300

    entrega do desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,91 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).send({ message: "Invalid repository ID." });
+  }
+  next();
+}
+
+function validateRepositoryExists(request, response, next) {
+  const { id } = request.params;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+  if (index < 0) {
+    return response.status(400).send({ message: "Repository not found" });
+  }
+
+  request.repositoryIndex = index;
+  request.repository = repositories[index];
+
+  next();
+}
+
+app.use("/repositories/:id", validateRepositoryId, validateRepositoryExists);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  if (title == undefined) {
+    return response.status(400).send({ message: "title is required" });
+  }
+
+  if (url == undefined) {
+    return response.status(400).send({ message: "url is required" });
+  }
+
+  if (techs == undefined || techs.length == 0 || !Array.isArray(techs))  {
+    return response.status(400).send({ message: "techs is required" });
+  }
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { repository } = request;
+
+  if (title != undefined) {
+    repository.title = title;
+  }
+
+  if (url != undefined) {
+    repository.url = url;
+  }
+
+  if (techs != undefined && techs.length > 0 && Array.isArray(techs))  {
+    repository.techs = techs;
+  }
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { repositoryIndex } = request;
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { repository } = request;
+  repository.likes++;
+  return response.json({likes: repository.likes});
 });
 
 module.exports = app;
